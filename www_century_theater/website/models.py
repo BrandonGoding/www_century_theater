8fefc47ce00299ac74a360a9ee1360639de910bc
Brandon H Goding <bgoding@pop-os.localdomain>
2022-03-12 21:39:14 -0500

    Now Playing Split Banner

--- www_century_theater/website/models.py
+++ www_century_theater/website/models.py
@@ -1,6 +1,10 @@
-from wagtail.admin.edit_handlers import StreamFieldPanel
+import datetime
+from django.db import models
+from wagtail.admin.edit_handlers import StreamFieldPanel, FieldPanel, MultiFieldPanel
+from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChooserPanel
 from blog.models import BlogPage
 from streams.blocks import ParallaxBlock, FeaturesListBlock, TeamHighlightBlock, RecentPostsBlock
 
@@ -24,3 +28,76 @@ class BasicPage(Page):
         context['post_row'] = BlogPage.objects.order_by('-post_date').live()[2:5]
         return context
 
+
+class Movie(Page):
+    RATING_CHOICES = (
+        ("G - General Audiences", "G"),
+        ("PG - Parental Guidance Suggested", "PG"),
+        ("PG-13 Parents Strongly Cautioned", "PG-13"),
+        ("NC-17 - Adults Only", "NC-17")
+    )
+
+    rating = models.CharField(max_length=35, choices=RATING_CHOICES, null=True, blank=True)
+    featured_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    youtube_id = models.CharField(max_length=25, null=True, blank=True)
+
+    content_panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel('title'),
+                FieldPanel('rating'),
+                ImageChooserPanel('featured_image'),
+                FieldPanel('youtube_id')
+            ],
+            heading="Movie Header Info"
+        ),
+    ]
+    parent_page_types = ['website.NowPlayingPage', 'website.ComingSoonPage']
+    subpage_types = []
+
+
+class NowPlayingPage(RoutablePageMixin, Page):
+    max_count = 1
+
+    subpage_types = ['website.Movie']
+
+    @property
+    def first_day_of_week(self):
+        current_day = datetime.date.today()
+        return current_day - datetime.timedelta(days=current_day.weekday()) + datetime.timedelta(days=4)
+
+    @property
+    def last_day_of_week(self):
+        current_day = datetime.date.today()
+        return current_day - datetime.timedelta(days=current_day.weekday()) + datetime.timedelta(days=6)
+
+    @route(r'^$')  # will override the default Page serving mechanism
+    def now_playing_page(self, request):
+        """
+        View function for the current events page
+        """
+        return self.render(request, context_overrides={})
+
+    def get_context(self, value, *args, **kwargs):
+        context = super(NowPlayingPage, self).get_context(value)
+        context['now_playing'] = Movie.objects.all().live()[:2]
+        return context
+
+
+class ComingSoonPage(RoutablePageMixin, Page):
+    max_count = 1
+
+    subpage_types = ['website.Movie']
+
+    @route(r'^$')  # will override the default Page serving mechanism
+    def coming_soon_page(self, request):
+        """
+        View function for the current events page
+        """
+        return self.render(request, context_overrides={})
